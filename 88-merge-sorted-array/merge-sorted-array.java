c153c7bee507df70b923c1a87ae807a0753ac964
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-03-16 19:41:46 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (57.09%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void merge(int[] n1, int m, int[] n2, int n) {
+        int k = m+n-1;
+
+        int i = m-1;
+        int j = n-1;
+
+        while(j >= 0){
+            if(i >= 0 && n1[i] >= n2[j])
+                n1[k--] = n1[i--];
+            else
+                n1[k--] = n2[j--];
+        }
+    }
+}
