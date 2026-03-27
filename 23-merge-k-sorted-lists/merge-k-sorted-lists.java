39c9a9bc739ec2d873d4d14862835f303acf64dc
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-03-27 12:59:29 +0530

    Time: 205 ms (5.03%) | Memory: 47 MB (22.52%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,77 +1,30 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length == 0)
-            return null;
-        if(lists.length == 1)
-            return lists[0];
+        int k = lists.length;
+        if (k == 0) return null;
 
-        ListNode dummy = new ListNode();
-        ListNode res = dummy;
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
 
-        int idx=0;
-        int c=0; // to check are we going circularly continously in case of all null
-        while(lists[idx] == null){
-            if(c == lists.length)
-                return null;
-            idx = (idx+1)%lists.length;
-            c++;
-        }
-        ListNode min = lists[idx];
-        int minListId = idx;
+        while (true) {
+            int minIndex = -1;
 
-        ListNode[] nextNode = new ListNode[lists.length];
+            for (int i = 0; i < k; i++) {
+                if (lists[i] == null) continue;
 
-        for(int i=0; i<lists.length; i++){
-            nextNode[i] = lists[i];
-        }
-        
-        masterloop:
-        while(true){
-
-            for(int i=0; i<lists.length; i++){
-                ListNode node = nextNode[i];
-                if(node != null && node.val <= min.val){
-                    min = node;
-                    minListId = i;
-                    // System.out.println(node.val + " " + min.val + " " + minListId);
+                if (minIndex == -1 || lists[i].val < lists[minIndex].val) {
+                    minIndex = i;
                 }
-
             }
-            ListNode newNode = new ListNode();
-            newNode.val = min.val;
-            res.next = newNode;
-            res = newNode;
-            nextNode[minListId]  = nextNode[minListId].next;
-            int count=0; // to check are we going circularly continously in case of all null
-            while(nextNode[minListId] == null){
-                if(count == lists.length)
-                    break masterloop;
-                minListId = (minListId+1)%lists.length;
-                count++;
-            }
-            min = nextNode[minListId];
 
-            //printing res list
-            // ListNode t = dummy.next;
-            // while(t!=null){
-            //     System.out.print(t.val + ", ");
-            //     t = t.next;
-            // }
-            // System.out.println("\n--");
+            if (minIndex == -1) break;
+
+            tail.next = lists[minIndex];
+            tail = tail.next;
+
+            lists[minIndex] = lists[minIndex].next;
         }
-        
 
         return dummy.next;
-
     }
 }
