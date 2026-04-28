f72355f9009f20ab4ec548d77974ee5092fbb171
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-04-28 17:47:03 +0530

    Time: 1 ms (94.64%) | Memory: 42.9 MB (69.77%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.java
@@ -1,35 +1,10 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int[] a = new int[nums.length];
-        int stopFlag = 1;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<nums.length; i++)
+            if(nums[i] > 0)
+                set.add(nums[i]);
 
-        for(int idx=0; idx<nums.length; idx++){
-            a[idx] = nums[idx];
-            if(stopFlag == 1 && a[idx] != 0) stopFlag = 0;
-        }
-
-        if(stopFlag == 1) return 0;
-
-        Arrays.sort(a);
-        int x = 0;
-
-        for(int c=1; ; c++){
-            while(a[x] == 0) x++;
-            for(int i=0; i<nums.length; i++){
-                if(nums[i] > 0)
-                    nums[i] -= a[x];
-                // System.out.println(nums[i] + ", ");
-            }
-
-            stopFlag = 1;
-            for(int i=0; i<nums.length; i++){
-                a[i] = nums[i];
-                if(stopFlag == 1 && a[i] != 0) stopFlag = 0;
-            }
-
-            if(stopFlag == 1) return c;
-            Arrays.sort(a);
-        }
-
+        return set.size();
     }
 }
