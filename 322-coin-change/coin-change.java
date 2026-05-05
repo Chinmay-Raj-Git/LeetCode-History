ea329cc3946ae384971eda85b2251bd2ee444bf7
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-05-05 21:17:39 +0530

    Time: 23 ms (31.64%) | Memory: 47.2 MB (20.14%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,28 +1,23 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int[][] dp = new int[coins.length+1][amount+1];
-        for(int i=0; i<=coins.length; i++)
-            Arrays.fill(dp[i], -1);
-
-        int ans = findMin(0, amount, coins, dp);
-        if(ans == Integer.MAX_VALUE) return -1;
-        return ans;
-    }
-
-    private int findMin(int i, int target, int[] coins, int[][] dp){
-        if(target == 0) return 0;
-        if(i>=coins.length) return Integer.MAX_VALUE;
-
-        if(dp[i][target] != -1) return dp[i][target];
-
-        int include = Integer.MAX_VALUE;
-        if(coins[i] <= target){
-            include = findMin(i, target-coins[i], coins, dp);
-            if(include != Integer.MAX_VALUE) include++;
+        for(int i=0; i<=coins.length; i++){
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+            dp[i][0] = 0;
         }
-        int exclude = findMin(i+1, target, coins, dp);
+            
+        for(int i=coins.length-1; i>=0; i--){
+            for(int target = 0; target<=amount; target++){
+                int include = Integer.MAX_VALUE;
+                if(coins[i] <= target){
+                    include = dp[i][target-coins[i]];
+                    if(include != Integer.MAX_VALUE) include++;
+                }
+                int exclude = dp[i+1][target];
+                dp[i][target] = Math.min(include, exclude);
+            }
+        }
 
-        dp[i][target] = Math.min(include, exclude);
-        return dp[i][target];
+        return dp[0][amount] == Integer.MAX_VALUE ? -1 : dp[0][amount];
     }
 }
