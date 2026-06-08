2c53d6c6d612da2f68d4d7afe2eb7c6b56d88ba4
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-06-08 23:16:57 +0530

    Time: 159 ms (19.81%) | Memory: 54.8 MB (8.65%) - LeetSync

---
+++ 127-word-ladder/word-ladder.java
@@ -0,0 +1,70 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> list) {
+        if(!list.contains(endWord)) return 0;
+        
+        HashSet<String> set = new HashSet<>();
+        for(String strin : list){
+            set.add(strin);
+        }
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        // for(int i=0; i<list.size(); i++){
+        //     map.put(list.get(i), getEdges(list.get(i), set));
+        // }
+        map.put(beginWord, getEdges(beginWord, set));
+
+        Queue<String> q = new LinkedList<>();
+        HashSet<String> vis = new HashSet<>();
+
+        q.add(beginWord);
+        vis.add(beginWord);
+
+        int steps = 1;
+
+        while(!q.isEmpty()){
+
+            int size = q.size();
+
+            for(int i = 0; i < size; i++){
+
+                String cur = q.poll();
+
+                if(cur.equals(endWord))
+                    return steps;
+
+                for(String nei : map.get(cur)){
+
+                    if(vis.contains(nei))
+                        continue;
+                    
+                    map.put(nei, getEdges(nei, set));
+                    vis.add(nei);
+                    q.add(nei);
+                }
+            }
+
+            steps++;
+        }
+
+        return 0;
+
+    }
+
+    private List<String> getEdges(String str, HashSet<String> set){
+        List<String> res = new ArrayList<>();
+        StringBuilder s = new StringBuilder(str);
+
+
+        for(int i=0; i<str.length(); i++){
+            for(char c = 'a'; c<='z'; c++){
+                s.setCharAt(i, c);
+                if(s.toString().equals(str)) continue;
+                if(set.contains(s.toString())) res.add(s.toString());
+                s.setCharAt(i, str.charAt(i));                 
+            }
+        }
+
+        return res;
+    }
+}
