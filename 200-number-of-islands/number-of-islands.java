3e615ea3a5afa75a853fcc0f51b970a51cffe39c
Chinmay-Raj-Git <ychinmayraj06@gmail.com>
2026-06-13 11:25:19 +0530

    Time: 9 ms (8.25%) | Memory: 51.3 MB (89.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,33 +1,33 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int[][] dir = {
-            {0,1}, {0,-1}, {1,0}, {-1,0}
+                { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 }
         };
         int r = grid.length;
         int c = grid[0].length;
         int res = 0;
 
-        Stack<int[]> stack = new Stack<>();
+        Queue<int[]> q = new LinkedList<>();
 
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == '1'){
-                    stack.push(new int[] {i, j});
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    q.offer(new int[] { i, j });
                     res++;
                 }
 
-                while(!stack.isEmpty()){
-                        int[] cur = stack.pop();
-                        for(int[] d : dir){
-                            int nr = cur[0] + d[0];
-                            int nc = cur[1] + d[1];
+                while (!q.isEmpty()) {
+                    int[] cur = q.poll();
+                    for (int[] d : dir) {
+                        int nr = cur[0] + d[0];
+                        int nc = cur[1] + d[1];
 
-                            if(nr<r && nc<c && nr>=0 && nc>=0 && grid[nr][nc] == '1'){
-                                grid[nr][nc] = '0';
-                                stack.push(new int[] {nr, nc});
-                            }
-
+                        if (nr < r && nc < c && nr >= 0 && nc >= 0 && grid[nr][nc] == '1') {
+                            grid[nr][nc] = '0';
+                            q.offer(new int[] { nr, nc });
                         }
+
+                    }
                 }
             }
 
